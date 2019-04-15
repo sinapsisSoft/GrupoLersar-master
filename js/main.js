ae4cfa0a02e998bfc672a87de5401efb37054e9a
Laura <lauramggarcia@hotmail.com>
2019-04-15 14:18:59 -0500

    Index Update
    Video, scroll up, social media removed

--- js/main.js
+++ js/main.js
@@ -161,3 +161,22 @@ jQuery(function($) {
 	
 });
 
+//Back Top Link
+var offset = 200;
+var duration = 500;
+$(window).scroll(function () {
+  if ($(this).scrollTop() > offset) {
+    $('.back-to-top').fadeIn(400);
+  } else {
+    $('.back-to-top').fadeOut(400);
+  }
+});
+
+$('.back-to-top').on('click', function (event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: 0
+  }, 600);
+  return false;
+})
+
